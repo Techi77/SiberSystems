8e82fd68c68e87627e8104364e209c5d59fb50a4
Katharine Rossinskaya <71548252+Techi77@users.noreply.github.com>
2022-02-28 13:37:25 +0300

    refactor: add errors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,14 @@ import java.io.File
 
 fun main() {
     val arrayOfCoordinates = readingTxtFile()
-    writingHTMLFile(arrayOfCoordinates)
+    try {
+        writingHTMLFile(arrayOfCoordinates)
+    } catch (e: NumberFormatException) {
+        println("Проверьте вводимые данные. Возможные ошибки:"
+                + "\n1. Данные не соотетствуют формату \"х1, у1; х2, у2;\"(в самом конце файла \";\" ставить не нужно)."
+                + "\n2. Прямоугольники перекрывают друг друга."
+                + "\n3. Указаны отрицательные значения")
+    }
 }
 
 fun readingTxtFile(): List<String> {
