390881be69ea87f824e0abab91d980b4c8b746dd
Katharine Rossinskaya <71548252+Techi77@users.noreply.github.com>
2022-02-28 14:04:08 +0300

    fix: add comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,12 @@ fun main() {
     try {
         writingHTMLFile(arrayOfCoordinates)
     } catch (e: NumberFormatException) {
-        println("Проверьте вводимые данные. Возможные ошибки:"
-                + "\n1. Данные не соотетствуют формату \"х1, у1; х2, у2;\"(в самом конце файла \";\" ставить не нужно)."
-                + "\n2. Прямоугольники перекрывают друг друга."
-                + "\n3. Указаны отрицательные значения")
+        println(
+            "Проверьте вводимые данные. Возможные ошибки:"
+                    + "\n1. Данные не соотетствуют формату \"х1, у1; х2, у2;\"(в самом конце файла \";\" ставить не нужно)."
+                    + "\n2. Прямоугольники перекрывают друг друга."
+                    + "\n3. Указаны отрицательные значения"
+        )
     }
 }
 
@@ -18,52 +20,72 @@ fun readingTxtFile(): List<String> {
 }
 
 fun writingHTMLFile(arrayOfCoordinates: List<String>) {
+
     val minXMinYMaxYArr = minXMinYMaxY(arrayOfCoordinates)
-    File("src/main/resources/result.html").writeText("<table>")
+    val minX = minXMinYMaxYArr[0]
+    val minY = minXMinYMaxYArr[1]
+    val maxY = minXMinYMaxYArr[2]
+
     var topPadding = ""
-    if (minXMinYMaxYArr[1] > 0) {
-        topPadding =
-            "\n  <tr>\n" + "    <td style=\"width: ${if (minXMinYMaxYArr[0] > 0) minXMinYMaxYArr[0] * 50 else 1}; height: ${minXMinYMaxYArr[1] * 50}\"></td>\n" + "  </tr>"
-    }
-    File("src/main/resources/result.html").appendText(topPadding)
+
     val arrayOfXAndY = divisionIntoXAndY(arrayOfCoordinates)
     val arrayOfX = arrayOfXAndY[0]
     val arrayOfY = arrayOfXAndY[1]
+
     var drawingLineNum: Int
-    val redLine = mutableListOf<String>()
+    val greenLine = mutableListOf<String>()
+
+    // Начало отрисовки
+    File("src/main/resources/result.html").writeText("<table>")
+
+    // Отрисовка прозрачного topPadding
+    if (minY > 0) {
+        topPadding =
+            "\n  <tr>\n" + "    <td style=\"width: ${if (minX > 0) minX * 50 else 1}; height: ${minY * 50}\"></td>\n" + "  </tr>"
+    }
+    File("src/main/resources/result.html").appendText(topPadding)
+
+    //отрисовка оставшейся части
     var i = 0
-    repeat(minXMinYMaxYArr[2] - minXMinYMaxYArr[1]) {
-        drawingLineNum = it + minXMinYMaxYArr[1]
+    repeat(maxY - minY) {
+        drawingLineNum = it + minY
         arrayOfY.forEach {
             if (drawingLineNum >= it.substringBefore(',').toInt() && drawingLineNum <= it.substringAfter(',').toInt()) {
-                if (drawingLineNum < arrayOfY[i].substringAfter(',').toInt()) redLine.add(arrayOfX[i])
+                if (drawingLineNum < arrayOfY[i].substringAfter(',').toInt()) greenLine.add(arrayOfX[i])
             }
             i += 1
         }
-        redLine.sort()
-        newLine(minXMinYMaxYArr[0], redLine)
+        greenLine.sort()
+        // отрисовка конкретной строки html таблицы
+        newLine(minX, greenLine)
+        // обнуление переменных
         i = 0
-        redLine.removeAll() { it.contains("") }
+        greenLine.removeAll() { it.contains("") }
     }
+
+    // Конец отрисовки
     File("src/main/resources/result.html").appendText("\n</table>")
 }
 
-fun newLine(minX: Int, redLine: MutableList<String>) {
+fun newLine(minX: Int, greenLine: MutableList<String>) {
     var lineInFile = "\n  <tr style=\"height: 50\">\n"
+    // left padding
     if (minX > 0) {
         lineInFile = "$lineInFile    <td></td>"
     }
-    if (redLine.size > 0 && (redLine[0].substringBefore(',').toInt() - minX) > 0) {
-        repeat(redLine[0].substringBefore(',').toInt() - minX) {
+    // прозрачный отступ
+    if (greenLine.size > 0 && (greenLine[0].substringBefore(',').toInt() - minX) > 0) {
+        repeat(greenLine[0].substringBefore(',').toInt() - minX) {
             lineInFile = "$lineInFile\n    <td style=\"width: 50\"></td>"
         }
     }
-    for (i in 0..redLine.lastIndex) {
-        repeat(redLine[i].substringAfter(',').toInt() - redLine[i].substringBefore(',').toInt()) {
+    // закрашенная часть
+    for (i in 0..greenLine.lastIndex) {
+        repeat(greenLine[i].substringAfter(',').toInt() - greenLine[i].substringBefore(',').toInt()) {
             lineInFile = "$lineInFile\n    <td style=\"background-color: green; width: 50\"></td>"
         }
-        if (i < redLine.lastIndex) {
-            repeat(redLine[i + 1].substringBefore(',').toInt() - redLine[i].substringAfter(',').toInt()) {
+        if (i < greenLine.lastIndex) {
+            repeat(greenLine[i + 1].substringBefore(',').toInt() - greenLine[i].substringAfter(',').toInt()) {
                 lineInFile = "$lineInFile\n    <td style=\"width: 50\"></td>"
             }
         }
